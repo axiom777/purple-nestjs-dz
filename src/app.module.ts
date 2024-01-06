b33199c25a91f3c10717f33de69a44b803e43c93
Evgenii Popov <axiom777@yandex.ru>
2024-01-06 15:53:03 +0300

    1-start feat: nestjs db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { ScheduleModule } from './schedule/schedule.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-	imports: [ScheduleModule, RoomsModule, AuthModule],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (config: ConfigService) => ({
+				uri: `mongodb://${config.get<string>('DB_HOST')}:${config.get<string>(
+					'DB_PORT',
+				)}/${config.get<string>('DB_NAME')}`,
+			}),
+		}),
+		ScheduleModule,
+		RoomsModule,
+		AuthModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
